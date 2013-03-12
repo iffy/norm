f34cb5ba241376375fbf2fb43bfd9b48e786d130
Matt Haggard <haggardii@gmail.com>
2013-03-12 15:03:48 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    url='https://github.com/iffy/norm',
+    author='Matt Haggard',
+    author_email='haggardii@gmail.com',
+    name='norm',
+    version='0.1',
+    packages=[
+        'norm', 'norm.test',
+    ],
+)
