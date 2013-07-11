744333d4f1a4f523c1e2ed09f20044daf66fa3ba
Matt Haggard <haggardii@gmail.com>
2013-07-11 16:05:57 -0600

    Update README with new ormHandle

--- norm/__init__.py
+++ norm/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Matt Haggard.
 # See LICENSE for details.
 
-__all__ = ['__version__', 'makePool', 'insert']
+__all__ = ['__version__', 'makePool', 'insert', 'ormHandle']
 
 __version__ = '1.4.0'
-from norm.porcelain import makePool, insert
+from norm.porcelain import makePool, insert, ormHandle
